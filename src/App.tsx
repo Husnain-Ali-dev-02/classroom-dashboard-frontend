03f8596d9facd840069db71f3eab66f2a55d1116
Husnain-Ali-Dev-002 <husnainali41940@gmail.com>
2026-01-15 15:29:41 +0500

    implement data provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Refine } from "@refinedev/core";
+import {  Refine  } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -6,17 +6,22 @@ import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
-import { dataProvider } from "./providers/data";
-import Dashboard from "./pages/dashboard";
-import { BookOpen, Home } from "lucide-react";
-import { Layout } from "./components/refine-ui/layout/layout";
+import Dashboard from "@/pages/dashboard.tsx";
+import {BookOpen, GraduationCap, Home} from "lucide-react";
+import {Layout} from "@/components/refine-ui/layout/layout.tsx";
 import SubjectsList from "@/pages/subjects/list.tsx";
 import SubjectsCreate from "@/pages/subjects/create.tsx";
+
+// import ClassesList from "@/pages/classes/list.tsx";
+// import ClassesCreate from "@/pages/classes/create.tsx";
+// import ClassesShow from "@/pages/classes/show.tsx";
+
+import {dataProvider} from "@/providers/data.ts";
 
 function App() {
   return (
@@ -31,36 +36,48 @@ function App() {
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
-                projectId: "qJn8z7-pO21On-9nnJnE",
+                projectId: "jtNqZy-1VTAjv-OXwtBn",
               }}
               resources={[
-                {
-                  name: "dashboard",
-                  list: "/",
-                  meta: { label: "Home", icon: <Home /> },
-                },
-                {
-                  name: "subjects",
-                  list: "/subjects",
-                  create: "/subjects/create",
-                  meta: { label: "Subjects", icon: <BookOpen /> },
-                },
+                  {
+                      name: 'dashboard',
+                      list: '/',
+                      meta: { label: 'Home', icon: <Home />}
+                  },
+                  {
+                      name: 'subjects',
+                      list: '/subjects',
+                      create: '/subjects/create',
+                      meta: { label: 'Subjects', icon: <BookOpen />}
+                  },
+                  {
+                      name: 'classes',
+                      list: '/classes',
+                      create: '/classes/create',
+                      show: '/classes/show/:id',
+                      meta: { label: 'Classes', icon: <GraduationCap />}
+                  }
               ]}
             >
               <Routes>
-                <Route
-                  element={
-                    <Layout>
-                      <Outlet />
-                    </Layout>
-                  }
-                ></Route>
+                  <Route element={
+                      <Layout>
+                          <Outlet />
+                      </Layout>
+                  }>
+                    <Route path="/" element={<Dashboard />} />
 
-                <Route path="/" element={<Dashboard />} />
-                <Route path="subjects">
-                  <Route index element={<SubjectsList />} />
-                  <Route path="create" element={<SubjectsCreate />} />
-                </Route>
+                      <Route path="subjects">
+                          <Route index element={<SubjectsList />} />
+                          <Route path="create" element={<SubjectsCreate />} />
+                      </Route>
+
+                      {/* <Route path="classes">
+                          <Route index element={<ClassesList />} />
+                          <Route path="create" element={<ClassesCreate />} />
+                          <Route path="show/:id" element={<ClassesShow />} />
+                      </Route> */}
+                  </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
